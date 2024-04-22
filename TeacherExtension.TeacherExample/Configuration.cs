620d7bb22bb7f4ef6270adb5b1e3ffca02df0365
Sakyceh <55030860+Sakyce@users.noreply.github.com>
2024-04-22 23:18:17 +0200

    Ajoutez des fichiers projet.

---
+++ TeacherExtension.TeacherExample/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx.Configuration;
+
+namespace TeacherExample
+{
+    public class Configuration
+    {
+        /// <summary>
+        /// The odds of the teacher spawning. Compared to the other teachers.
+        /// <para>
+        /// Examples : 
+        /// <code>
+        /// Baldi : 100
+        /// Foxo  : 100
+        /// Null  : 20
+        /// </code>
+        /// </para>
+        /// </summary>
+        public static ConfigEntry<int> SpawnWeight { get; internal set; }
+
+        internal static void Setup()
+        {
+            SpawnWeight = ExampleTeacherPlugin.Instance.Config.Bind(
+                "ExampleTeacher", 
+                "SpawnWeight", 
+                10000,
+                "More it is higher, more there is a chance of him spawning. (Defaults to 100. For comparison, Baldi weight is 100) (Requires Restart)"
+            );
+        }
+    }
+}
---
+++ TeacherExtension.TeacherExample/ExampleTeacher.cs
@@ -0,0 +1,92 @@
+﻿using MTM101BaldAPI.Registers;
+using TeacherAPI;
+using UnityEngine;
+
+namespace TeacherExample
+{
+    public class ExampleTeacher : Teacher
+    {
+        // As Teacher inherits from Baldi, Make sure to override the method with this if you don't want his slap mechanic.
+        // (If you forget, the teacher won't move at all)
+        public override float DistanceCheck(float val) => val;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            var beans = (Beans)NPCMetaStorage.Instance.Get(Character.Beans).value;
+
+            // Add a sound when CaughtPlayer is called
+            AddLoseSound(beans.audNPCHitSounds[0], 100);
+
+            // Replace the event texts (The ones naturally generated, doesn't works with mod menus) with custom
+            ReplaceEventText<RulerEvent>("Beans lost his bubble gum.");
+            ReplaceEventText<PartyEvent>("Party!!!!");
+
+            // No arguments = No music
+            ReplaceMusic();
+
+            // Very important!!!
+            behaviorStateMachine.ChangeState(new Example_Happy(this));
+        }
+    }
+
+    /// <summary>
+    /// A base for all your custom teacher state, you will need it if your teacher has custom fields.
+    /// </summary>
+    public class Example_StateBase : TeacherState
+    {
+        public Example_StateBase(ExampleTeacher example) : base(example)
+        {
+            this.example = example;
+        }
+        protected ExampleTeacher example;
+    }
+
+    /// <summary>
+    /// The starting state the teacher will be at the start of the floor. Used as a gateway between Happy and Angry too.
+    /// </summary>
+    public class Example_Happy : Example_StateBase
+    {
+        public Example_Happy(ExampleTeacher example) : base(example) { }
+        public override void PlayerExitedSpawn()
+        {
+            // Will change state when the player exits the spawn, like Baldi when starting to count.
+            base.PlayerExitedSpawn();
+            example.behaviorStateMachine.ChangeState(new Example_Chase(example));
+            example.ActivateSpoopMode(); // Ctrl + Click the method name to see why it's important
+        }
+    }
+
+    /// <summary>
+    /// The angry state of the teacher.
+    /// </summary>
+    public class Example_Chase : Example_StateBase
+    {
+        public Example_Chase(ExampleTeacher example) : base(example) { }
+
+        public override void Enter()
+        {
+            base.Enter();
+            Debug.Log("IT's cRAZY TiME!");
+            ChangeNavigationState(new NavigationState_WanderRandom(example, 0)); // For reference, PartyEvent has a priority of 31.
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            example.Navigator.SetSpeed(example.Navigator.Speed + Time.deltaTime * 100f);
+        }
+
+
+        public override void OnStateTriggerStay(Collider other)
+        {
+            // Obvious
+            if (example.IsTouchingPlayer(other))
+            {
+                example.CaughtPlayer(other.GetComponent<PlayerManager>());
+            }
+        }
+    }
+
+}
